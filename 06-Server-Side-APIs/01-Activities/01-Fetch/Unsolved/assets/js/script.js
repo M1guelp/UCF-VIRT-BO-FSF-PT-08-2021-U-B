2d044204f61587df207903a2a9be982aff43e6d9
miguel puerto <miguelp3024@gmail.com>
2021-09-28 18:54:27 -0400

    cahnges

--- 06-Server-Side-APIs/01-Activities/01-Fetch/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/01-Fetch/Unsolved/assets/js/script.js
@@ -1,2 +1,25 @@
 // Write a fetch request to the Giphy API
 // Then log the response in the console
+// fetch('https://api.giphy.com/v1/gifs/trending?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN', {
+//   credentials: 'include'
+// });
+
+// addEventListener.click (
+//     'btn'
+// )
+
+
+
+var giphyButton = document.getElementById('giphy-button');
+
+giphyButton.addEventListener('click', function(event){
+
+  
+fetch('https://api.chucknorris.io/jokes/random')
+  .then(function(response) { 
+    return response.json()
+  })
+  .then(function(data) {
+    console.log(data)
+  });
+})
